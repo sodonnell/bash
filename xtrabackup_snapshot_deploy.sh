dd84ff45d007b78793455818f07d83775474f35f
Sean O'Donnell <sean@seanodonnell.com>
2017-10-17 11:29:49 -0700

    Update xtrabackup_snapshot_deploy.sh

--- xtrabackup_snapshot_deploy.sh
+++ xtrabackup_snapshot_deploy.sh
@@ -3,12 +3,12 @@
 # xtrabackup_snapshot_deploy.sh
 #
 # This script is intended to perform database 'rollback' procedures 
-# from existing a specified xtrabackup snapshot archive. This script 
-# is ideal for installing a production snapshot on our staging database,
-# or slave instance.
+# from a specified xtrabackup snapshot archive. This script 
+# is ideal for installing a production snapshot on a staging database,
+# local workstation, or replication-slave instance.
 #
-# This script assumes that you have sudo access to: 
-# 'service, mkdir, and chown', respectively. 
+# This script assumes that you have sudo access, and was developed
+# to support CentOS (and other RHEL-based distros).
 #
 # Author: Sean O'Donnell <sean@seanodonnell.com>
 #
@@ -26,13 +26,13 @@ if [ -e $ARCHIVEPATH ]; then
 	
 	# move existing database files to a back-up directory, in case you 
 	# want to roll-back from your attempted snapshot rollback/deployment
-	mv $DBPATH $DBPATH.bak-$DATESTAMP
+	sudo mv $DBPATH $DBPATH.bak-$DATESTAMP
 	
 	# recreate the database directory
 	sudo mkdir $DBPATH
 	
 	# recover the data from innobackupex
-	innobackupex --copy-back $ARCHIVEPATH
+	sudo innobackupex --copy-back $ARCHIVEPATH
 	
 	# change ownership of files to database user (mysql)
 	sudo chown -R mysql:mysql $DBPATH
